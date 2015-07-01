218be874022ae2b1a0219b873607268cff6cf455
Grant Shively <gshively@godaddy.com>
2015-07-01 12:17:33 -0700

    Removed version constraint so that consumers can constrain the version in their environment/wrapper cookbooks

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'gba@onbeep.com'
 license          'The MIT License (MIT)'
 description      'SSL key & certificate storage in chef-vault.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '1.1.1'
+version          '2.0.0'
 
 supports 'ubuntu'
 
-depends 'chef-vault', '~> 1.0.4'
+depends 'chef-vault'
